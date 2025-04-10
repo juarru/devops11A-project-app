53897754772a5993a2208c4ccc56b28265fbd55c
Juan Arillo <juan.arillo@apppadel.com>
2025-04-10 13:01:59 +0200

    feat: added health endpoint and added counter for prometheus. added new index in elastic search

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ import json
 import time
 from elasticsearch import Elasticsearch
 from datetime import datetime
+from prometheus_client import Counter
 
 app = Flask(__name__)
 
@@ -37,9 +38,15 @@ REDIS_DB = int(os.getenv("REDIS_DB", "0"))
 # Redis connection
 db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
 
-# Configuración de Elasticsearch
+# Elasticsearch configuration
 ELASTICSEARCH_HOST = os.getenv('ELASTICSEARCH_HOST', 'elasticsearch')
 es = Elasticsearch(f"http://{ELASTICSEARCH_HOST}:9200")
+
+# Prometheus metrics
+REQUESTS = Counter('server_requests_total', 'Total number of requests to this webserver')
+HEALTHCHECK_REQUESTS = Counter('healthcheck_requests_total', 'Total number of requests to healthcheck')
+MAIN_ENDPOINT_REQUESTS = Counter('main_requests_total', 'Total number of requests to main endpoint')
+LOGS_ENDPOINT_REQUESTS = Counter('logs_requests_total', 'Total number of requests to logs endpoint')
 
 # Error: Uncomment to see how pylint works.
 # x = "This variable has a very short name"
@@ -73,6 +80,8 @@ def index():
     try:
         es.index(index="flask-logs", document=log_message)
         logger.info(f"Datos almacenados en Elasticsearch: {log_message}")
+        REQUESTS.inc()
+        MAIN_ENDPOINT_REQUESTS.inc()
     except Exception as e:
         logger.error(f"Error al almacenar en Elasticsearch: {e}")
         return jsonify({"error": str(e)}), 500
@@ -80,17 +89,43 @@ def index():
 
     return f"La página ha sido cargada {count} veces."
 
+@app.route("/health", methods=["GET"])
+def health_check():
+    """Implement health check endpoint"""
+
+    logger.info("Healthcheck request received")
+
+    log_message = {
+        "message": "Request a health recibido",
+        "timestamp": datetime.now().isoformat()
+    }
+
+    try:
+        es.index(index="health-logs", document=log_message)
+        logger.info(f"Llamada a health almacenada: {log_message}")
+
+        REQUESTS.inc()
+        HEALTHCHECK_REQUESTS.inc()
+    except Exception as e:
+        logger.error(f"Error de llamada a health: {e}")
+        return jsonify({"error": str(e)}), 500
+    
+    return {"health": "ok"}
+
 @app.route('/logs', methods=['GET'])
 def get_logs():
     """
     Recupera los datos almacenados en Elasticsearch.
     """
-    index_name = "flask-logs"
     try:
-        response = es.search(index=index_name, query={"match_all": {}})
+        response = es.search(index=["flask-logs", "health-logs"], query={"match_all": {}})
         hits = response.get("hits", {}).get("hits", [])
         logs = [hit["_source"] for hit in hits]
         logger.info("Datos recuperados desde Elasticsearch")
+
+        REQUESTS.inc()
+        LOGS_ENDPOINT_REQUESTS.inc()
+
         return jsonify(logs), 200
     except Exception as e:
         logger.error(f"Error al recuperar datos desde Elasticsearch: {e}")
